a2b0be41847b827e9ccd61bff29041075aec9052
vang794 <vang794@uwm.edu>
2025-02-05 22:23:27 -0600

    Remade models for user and roles, made sure user model was in admin.py, made sure polls is included in installed_apps in settings.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User  # Import the models
+
+# Register your models here
+admin.site.register(User)
--- polls/models.py
+++ polls/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Roles(models.TextChoices):
-    Event_Manager = 'Event_Manager'
-    User = 'User'
 
-class User(models.TextChoices):
-    #id can also be username
-    id = models.CharField(max_length=20,primary_key=True)
+class Roles(models.TextChoices):
+    EVENT_MANAGER = 'Event_Manager', 'Event Manager'  # First is stored value, second is human-readable
+    USER = 'User', 'User'
+
+class User(models.Model):
+    id = models.CharField(max_length=20, primary_key=True, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
-    phone_number = models.IntegerField()
-    role = models.CharField(max_length=50, choices=Roles)
+    phone_number = models.PositiveIntegerField(null=True, blank=True)
+    role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.USER)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.role})"
