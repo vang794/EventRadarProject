5fc49ab2320f1f76b3845c87235459d52c6880a9
hackba74 <hackba74@uwm.edu>
2025-02-05 20:33:00 -0600

    created base methods for create_account

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import CreateAccountForm
 
-# Create your views here.
+def create_account(request):
+    if request.method == 'POST':
+        form = CreateAccountForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('account_created'))
+        return render(request, 'polls/create_account.html', {'form': form})
+    
+    form = CreateAccountForm()
+    return render(request, 'polls/create_account.html', {'form': form})
+
+def account_created(request):
+    return render(request, 'polls/account_created.html')
