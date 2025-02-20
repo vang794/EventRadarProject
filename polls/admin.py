982ee1797320a45daf4db6a086aeb8b72cc8e7df
vang794 <vang794@uwm.edu>
2025-02-20 14:02:38 -0600

    altered database changes

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-
 # Register your models here.
 from django.contrib import admin
-from .models import User  # Import the models
+from .models import User
 
-# Register your models here
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username')
+    readonly_fields = ('id',)
+
+admin.site.register(User, UserAdmin)
+
