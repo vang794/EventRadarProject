0ee35654dad3aad89845f54649f2a1aff6dd5752
AnnaliseHarms <akharms@uwm.edu>
2025-02-20 23:41:11 -0600

    migrations done- not sure correct

--- polls/models.py
+++ polls/models.py
@@ -17,6 +17,5 @@ class User(models.Model):
     phone_number = models.PositiveIntegerField(null=True, blank=True)
     role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.USER)
 
-
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.role})"
